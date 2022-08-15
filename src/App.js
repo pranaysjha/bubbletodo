eacf12b2a7483f9b1d7c7818c9d1c13ef9402e95
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-08-15 11:55:32 -0700

    Footer in place; review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className="">
       <NavBar />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import '../input.css'
+
+const Footer = () => {
+    return (
+        <footer className="absolute bottom-0 left-0 footer footer-center p-2 text-base-content">
+            <div>
+                <p className="text-xs text-[#3ABFF8]/25">Copyright © 2022 Ben Garofalo and Pranay Jha</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
