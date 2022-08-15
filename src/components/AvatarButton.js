d8abd22fe8aadf3bc051497f7223e7bfda2d1ced
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-08-15 01:38:21 -0700

    NavBar, AvatarButton UI basically done (daisyUI is goated), AddBubbleButton WIP

---
+++ src/components/AvatarButton.js
@@ -0,0 +1,19 @@
+import '../input.css';
+
+const AvatarButton = () => {
+  return (
+		<div className="dropdown dropdown-end">
+			<label tabindex="0" className="btn btn-ghost btn-circle avatar">
+				<div className="w-10 rounded-full">
+					<img src="https://placeimg.com/80/80/people" alt=""/>
+				</div>
+			</label>
+			<ul tabindex="0" className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+				<li><a href="https://calendar.google.com/calendar/u/0/r" target="_blank" rel="noreferrer">Google Calendar</a></li>
+				<li><a href="#" onClick={() => {return false;}}>Sign out</a></li>
+			</ul>
+		</div>
+  );
+}
+
+export default AvatarButton;
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import '../input.css';
+import AddBubbleButton from "./AddBubbleButton";
+import AvatarButton from "./AvatarButton";
+
+const NavBar = () => {
+	return (
+		<div className="navbar bg-neutral drop-shadow-md">
+			<div className="flex-1">
+				<a href="." className="btn btn-ghost normal-case text-3xl">Logo</a>
+			</div>
+			<div className="flex-none gap-4">
+				<AddBubbleButton />
+				<AvatarButton />
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
