ab7ccba812f1c9af80a599d31c90c78379cfa958
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2022-10-06 01:31:48 -0700

    Bubble -> BubbleTasks cuz why not

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = (props) => {
 	return (
 		<div id="navbar" className="navbar absolute z-50">
 			<div className="flex-1">
-				<a href="." className="btn btn-ghost normal-case text-3xl">Bubble</a>
+				<a href="." className="btn btn-ghost normal-case text-3xl">BubbleTasks</a>
 			</div>
 			<div className="flex-none gap-4">
 				<AvatarButton user={props.user}/>
