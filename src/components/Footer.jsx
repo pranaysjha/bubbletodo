e6ab983fa191269c55499ad28ecaa7f53cdac67f
Ben Garofalo <bentgarofalo@gmail.com>
2022-12-21 11:02:48 -0800

    policy page not working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
         <footer id="footer" className="absolute bottom-0 left-0 footer footer-center p-2 text-base-content">
             <div>
                 <p className="text-xs text-[#3ABFF8]/25">Copyright © 2022 Ben Garofalo and Pranay Jha</p>
+                <a href="/priv.html">Privacy Polici</a>
             </div>
         </footer>
     );
