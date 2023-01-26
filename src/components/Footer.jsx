5ec3e0f0dde2da60391b8b387b0980510f825ac9
pranay-jha <63332027+pranay-jha@users.noreply.github.com>
2023-01-25 16:28:23 -0800

    copyright update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div>
             <footer id="footer" className="absolute bottom-0 left-0 footer footer-center p-2 text-base-content">
                 <div className="inline-block">
-                    <p className="text-xs text-[#3ABFF8]/25 inline-block mx-2">Copyright © 2022 Ben Garofalo and Pranay Jha</p>
+                    <p className="text-xs text-[#3ABFF8]/25 inline-block mx-2">Copyright © 2023 Ben Garofalo and Pranay Jha</p>
                     <a className="text-xs text-[#3ABFF8]/25 underline inline-block" href="privacypolicy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
                 </div>
             </footer>
